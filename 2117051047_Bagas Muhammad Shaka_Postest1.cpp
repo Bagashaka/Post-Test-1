1445edba69c670454fad84e5f80ed2873e7d01b5
Bagas Muhammad Shaka <89391428+Bagashaka@users.noreply.github.com>
2021-11-02 14:58:34 +0800

    Add files via upload

---
+++ 2117051047_Bagas Muhammad Shaka_Postest1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Nama:Bagas Muhammad Shaka                                                                            6 September 2021" << endl;
+	cout << "    NPM: 2117051047      " << endl;
+	cout << "================================================================" << endl;
+	string a , b, c;
+	
+	
+	cout << "Masukkan angka ke dalam konstanta Persamaan kuadrat Dibawah ini!" << endl;
+	cout << "                        ax^2 + bx + c                           " << endl;
+	cout << "================================================================" << endl;
+	
+	cout << "Masukkan Nilai a, b ,c: ";
+	cin >> a >> b >> c;
+	
+	cout << "================================================================" << endl;
+    cout << "                   INI HASIL INPUTAN                            " << endl;
+	cout << a << "x^2 +" << b << " x + " << c;
+	return 0;
+	
+	
+	
+	
+
+	
+	
+	
+}
